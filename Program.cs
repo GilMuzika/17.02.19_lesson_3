3c2486b5eeaf589b4b5daabe362bd561489642a1
GilMuzika <47449752+GilMuzika@users.noreply.github.com>
2019-02-19 19:29:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19._02._19_lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first number:");
+            int b = Int32.Parse(Console.ReadLine());
+
+            if (a > b) { Console.WriteLine(a); }
+            else { Console.WriteLine(b); }
+
+
+
+        }
+    }
+}
